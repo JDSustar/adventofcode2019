7f9cba3d6702df57e9fec41c99ed5b4d7e3fd57e
Jason Sustar <jason.sus@gmail.com>
2019-12-03 21:50:06 -0500

    More Day 3 Tests

--- AdventOfCode2019Test/Day3Tests.cs
+++ AdventOfCode2019Test/Day3Tests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using AdventOfCode2019;
-using AdventOfCode2019.Day3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode2019Test
@@ -127,6 +126,28 @@ namespace AdventOfCode2019Test
             WirePanel wp = new WirePanel(wire1, wire2);
 
             Assert.AreEqual(135, wp.GetClosestIntersectionDistance());
+        }
+
+        [TestMethod]
+        public void Day3FewestStepsExample1()
+        {
+            string wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+            string wire2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+
+            WirePanel wp = new WirePanel(wire1, wire2);
+
+            Assert.AreEqual(610, wp.GetFewestStepsIntersection());
+        }
+
+        [TestMethod]
+        public void Day3FewestStepsExample2()
+        {
+            string wire1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+            string wire2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+
+            WirePanel wp = new WirePanel(wire1, wire2);
+
+            Assert.AreEqual(410, wp.GetFewestStepsIntersection());
         }
     }
 }
