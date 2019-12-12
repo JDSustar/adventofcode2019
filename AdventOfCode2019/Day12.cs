2c806a1fbb1c68dd6592f8675c6b00a77a44b5e1
Jason Sustar <jason.sus@gmail.com>
2019-12-12 12:14:59 -0500

    Day 12.1

---
+++ AdventOfCode2019/Day12.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    public static class Day12
+    {
+        public static void ExecuteStarOne(string fileLocation = "PuzzleInput/Day12.txt")
+        {
+            List<string> moonStringList = File.ReadAllLines(fileLocation).ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                jm.Tick();
+            }
+
+            Logger.LogMessage(LogLevel.ANSWER, "12A: Jupiter Moon Energy: " + jm.GetSystemEnergy());
+        }
+
+        public static void ExecuteStarTwo(string fileLocation = "PuzzleInput/Day12.txt")
+        {
+            List<string> moonStringList = File.ReadAllLines(fileLocation).ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+            var initialJm = jm.Clone() as JupiterMoons;
+            jm.Tick();
+
+            long steps = 0;
+
+            while (!jm.Equals(initialJm))
+            {
+                jm.Tick();
+                steps++;
+                if (steps % 1000 == 0) Logger.LogMessage(LogLevel.DEBUG, steps.ToString());
+            }
+
+            Logger.LogMessage(LogLevel.ANSWER, "12B: Steps to Repeat: " + steps);
+        }
+    }
+
+    public class JupiterMoons : ICloneable
+    {
+        public List<Moon> Moons { get; } = new List<Moon>();
+
+        public JupiterMoons(List<string> moonStrings)
+        {
+            foreach (var moonString in moonStrings)
+            {
+                Moons.Add(new Moon(moonString));
+            }
+        }
+
+        public JupiterMoons(JupiterMoons jm)
+        {
+            foreach (var jmMoon in jm.Moons)
+            {
+                this.Moons.Add((jmMoon.Clone() as Moon));
+            }
+        }
+
+        public void Tick()
+        {
+            foreach (var moon in Moons)
+            {
+                foreach (var moon1 in Moons)
+                {
+                    moon1.AdjustVelocityFromGravityOf(moon);
+                }
+            }
+
+            foreach (var moon in Moons)
+            {
+                moon.Tick();
+            }
+        }
+
+        public int GetSystemEnergy()
+        {
+            int energy = 0;
+
+            foreach (var moon in Moons)
+            {
+                energy += moon.GetPotentialEnergy() * moon.GetKineticEnergy();
+            }
+
+            return energy;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is JupiterMoons jm)
+            {
+                if (this.Moons.Count != jm.Moons.Count)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < this.Moons.Count; i++)
+                {
+                    if (!this.Moons[i].Equals(jm.Moons[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Moons.Sum(m => m.GetHashCode());
+        }
+
+        public object Clone()
+        {
+            return new JupiterMoons(this);
+        }
+    }
+
+    public class Moon : ICloneable
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public int XVelocity { get; private set; }
+        public int YVelocity { get; private set; }
+        public int ZVelocity { get; private set; }
+
+        public Moon(string input)
+        {
+            string coordString = input;
+            coordString = coordString.Substring(1).Remove(coordString.Length - 2);
+
+            string[] coordsStrings = coordString.Split(',');
+
+            X = int.Parse(coordsStrings[0].Trim().Substring(2));
+            Y = int.Parse(coordsStrings[1].Trim().Substring(2));
+            Z = int.Parse(coordsStrings[2].Trim().Substring(2));
+
+            XVelocity = 0;
+            YVelocity = 0;
+            ZVelocity = 0;
+        }
+
+        public Moon(Moon m)
+        {
+            this.X = m.X;
+            this.Y = m.Y;
+            this.Z = m.Z;
+
+            this.XVelocity = m.XVelocity;
+            this.YVelocity = m.YVelocity;
+            this.ZVelocity = m.ZVelocity;
+        }
+
+        public void AdjustVelocityFromGravityOf(Moon m)
+        {
+            if (m.X > this.X)
+            {
+                XVelocity++;
+            }
+            else if (m.X < this.X)
+            {
+                XVelocity--;
+            }
+
+            if (m.Y > this.Y)
+            {
+                YVelocity++;
+            }
+            else if (m.Y < this.Y)
+            {
+                YVelocity--;
+            }
+
+            if (m.Z > this.Z)
+            {
+                ZVelocity++;
+            }
+            else if (m.Z < this.Z)
+            {
+                ZVelocity--;
+            }
+        }
+
+        public void Tick()
+        {
+            X += XVelocity;
+            Y += YVelocity;
+            Z += ZVelocity;
+        }
+
+        public int GetPotentialEnergy()
+        {
+            return Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+        }
+
+        public int GetKineticEnergy()
+        {
+            return Math.Abs(XVelocity) + Math.Abs(YVelocity) + Math.Abs(ZVelocity);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Moon m)
+            {
+                return this.X == m.X && this.Y == m.Y && this.Z == m.Z && this.XVelocity == m.XVelocity && this.YVelocity == m.YVelocity && this.ZVelocity == m.ZVelocity;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.X + this.Y + this.Z).GetHashCode();
+        }
+
+        public object Clone()
+        {
+            return new Moon(this);
+        }
+    }
+}
---
+++ AdventOfCode2019Test/Day12Tests.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2019;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2019Test
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            Assert.AreEqual(4, jm.Moons.Count);
+
+            Assert.AreEqual(-1, jm.Moons[0].X);
+            Assert.AreEqual(0, jm.Moons[0].Y);
+            Assert.AreEqual(2, jm.Moons[0].Z);
+
+            Assert.AreEqual(2, jm.Moons[1].X);
+            Assert.AreEqual(-10, jm.Moons[1].Y);
+            Assert.AreEqual(-7, jm.Moons[1].Z);
+
+            Assert.AreEqual(4, jm.Moons[2].X);
+            Assert.AreEqual(-8, jm.Moons[2].Y);
+            Assert.AreEqual(8, jm.Moons[2].Z);
+
+            Assert.AreEqual(3, jm.Moons[3].X);
+            Assert.AreEqual(5, jm.Moons[3].Y);
+            Assert.AreEqual(-1, jm.Moons[3].Z);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            for (int i = 0; i < 10; i++)
+            {
+                jm.Tick();
+            }
+
+            Assert.AreEqual(2, jm.Moons[0].X);
+            Assert.AreEqual(1, jm.Moons[0].Y);
+            Assert.AreEqual(-3, jm.Moons[0].Z);
+
+            Assert.AreEqual(1, jm.Moons[1].X);
+            Assert.AreEqual(-8, jm.Moons[1].Y);
+            Assert.AreEqual(0, jm.Moons[1].Z);
+
+            Assert.AreEqual(3, jm.Moons[2].X);
+            Assert.AreEqual(-6, jm.Moons[2].Y);
+            Assert.AreEqual(1, jm.Moons[2].Z);
+
+            Assert.AreEqual(2, jm.Moons[3].X);
+            Assert.AreEqual(0, jm.Moons[3].Y);
+            Assert.AreEqual(4, jm.Moons[3].Z);
+
+            Assert.AreEqual(-3, jm.Moons[0].XVelocity);
+            Assert.AreEqual(-2, jm.Moons[0].YVelocity);
+            Assert.AreEqual(1, jm.Moons[0].ZVelocity);
+
+            Assert.AreEqual(-1, jm.Moons[1].XVelocity);
+            Assert.AreEqual(1, jm.Moons[1].YVelocity);
+            Assert.AreEqual(3, jm.Moons[1].ZVelocity);
+
+            Assert.AreEqual(3, jm.Moons[2].XVelocity);
+            Assert.AreEqual(2, jm.Moons[2].YVelocity);
+            Assert.AreEqual(-3, jm.Moons[2].ZVelocity);
+
+            Assert.AreEqual(1, jm.Moons[3].XVelocity);
+            Assert.AreEqual(-1, jm.Moons[3].YVelocity);
+            Assert.AreEqual(-1, jm.Moons[3].ZVelocity);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            for (int i = 0; i < 10; i++)
+            {
+                jm.Tick();
+            }
+
+            Assert.AreEqual(6, jm.Moons[0].GetPotentialEnergy());
+            Assert.AreEqual(9, jm.Moons[1].GetPotentialEnergy());
+            Assert.AreEqual(10, jm.Moons[2].GetPotentialEnergy());
+            Assert.AreEqual(6, jm.Moons[3].GetPotentialEnergy());
+
+            Assert.AreEqual(6, jm.Moons[0].GetKineticEnergy());
+            Assert.AreEqual(5, jm.Moons[1].GetKineticEnergy());
+            Assert.AreEqual(8, jm.Moons[2].GetKineticEnergy());
+            Assert.AreEqual(3, jm.Moons[3].GetKineticEnergy());
+
+
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            for (int i = 0; i < 10; i++)
+            {
+                jm.Tick();
+            }
+
+            Assert.AreEqual(179, jm.GetSystemEnergy());
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            var jm2 = (jm.Clone() as JupiterMoons);
+
+            bool m = jm.Equals(jm2);
+
+            Assert.AreEqual(jm, jm2);
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            string input = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            List<string> moonStringList = input.Split("\r\n").ToList();
+
+            var jm = new JupiterMoons(moonStringList);
+
+            var states = new HashSet<JupiterMoons>();
+
+            long steps = 0;
+
+            bool repeatFound = false;
+
+            JupiterMoons jm2 = null;
+
+            while (!repeatFound)
+            {
+                jm.Tick();
+
+                if (states.TryGetValue(jm, out jm2))
+                {
+                    repeatFound = true;
+                }
+                else
+                {
+                    states.Add((jm.Clone() as JupiterMoons));
+                    steps++;
+                }
+            }
+
+            Assert.AreEqual(jm, jm2);
+            Assert.AreEqual(2772, steps);
+        }
+    }
+}
